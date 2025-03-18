71a7d6890c106777f31338e0145164f4a7939419
nagdista <nagdista@gmail.com>
2025-03-18 06:30:06 +0200

    day 18

--- README.md
+++ README.md
@@ -45,6 +45,14 @@
 
 ## day
 
+- 18 March 2025
+- Count by X
+- [link](https://www.codewars.com/kata/5513795bd3fafb56c200049e/javascript)
+- notes : Learn `Array.from` , `Array.key`
+---
+
+## day
+
 -
 -
 - [link]()
---
+++ day15.js
@@ -0,0 +1,22 @@
+// ! My solve 
+/* 
+function countBy(x, n) {
+    let z = [];
+    let cntr ; 
+
+    for(i=1 ; i<=n ; i++){
+        cntr = x; 
+       z.push(i*cntr);
+       console.log(cntr);
+       
+    }
+  
+    return z;
+  }
+  
+console.log(countBy(2,5)); // [2,4,6,8,10]
+*/
+
+// ! MY solve after learn `array.from `
+
+
